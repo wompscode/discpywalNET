feb65fc5b0768e626067a6cb3697147e64b4770f
womp <24914325+wompscode@users.noreply.github.com>
2025-02-21 01:16:35 +1100

    Initial commit

---
+++ discpywalNET/Structs.cs
@@ -0,0 +1,19 @@
+﻿using Discord;
+
+namespace TimCSweeney;
+
+public static class Structs
+{
+    public struct RegEx
+    {
+        public string Pattern { get; init; }
+        public string Emote { get; init; }
+        public bool CustomEmoji { get; init; }
+    }
+
+    public struct Activity
+    {
+        public ActivityType Type { get; init; }
+        public string Text { get; init; }
+    }
+}
